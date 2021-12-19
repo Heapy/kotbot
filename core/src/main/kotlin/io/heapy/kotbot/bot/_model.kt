7314033224e419962733d87abe76b5e2aeaa8e28
Ruslan Ibragimov <ruslan@heapy.io>
2021-12-20 01:47:13 +0300

    All API types should be data classes.

--- core/src/main/kotlin/io/heapy/kotbot/bot/_model.kt
+++ core/src/main/kotlin/io/heapy/kotbot/bot/_model.kt
@@ -378,21 +378,6 @@ public data class Update(
     @Serializable
     public class ChatJoinRequest()
     @Serializable
-    public class Chat(
-        /**
-         * Unique identifier for this chat. This number may have more than 32
-         * significant bits and some programming languages may have
-         * difficulty/silent defects in interpreting it. But it has at most
-         * 52 significant bits, so a signed 64-bit integer or double-precision
-         * float type are safe for storing this identifier.
-         */
-        public val id: Long,
-        /**
-         * Type of chat, can be either “private”, “group”, “supergroup” or “channel”
-         */
-        public val type: String,
-    )
-    @Serializable
     public class MessageEntity()
     @Serializable
     public class Animation()
@@ -447,6 +432,41 @@ public data class Update(
     @Serializable
     public class InlineKeyboardMarkup()
 }
+
+/**
+ * This object represents a chat.
+ */
+@Serializable
+public data class Chat(
+    /**
+     * Unique identifier for this chat. This number may have more than 32
+     * significant bits and some programming languages may have
+     * difficulty/silent defects in interpreting it. But it has at most
+     * 52 significant bits, so a signed 64-bit integer or double-precision
+     * float type are safe for storing this identifier.
+     */
+    public val id: Long,
+    /**
+     * Type of chat, can be either “private”, “group”, “supergroup” or “channel”
+     */
+    public val type: String,
+    /**
+     * Optional. Title, for supergroups, channels and group chats
+     */
+    public val title: String? = null,
+    /**
+     * Optional. Username, for private chats, supergroups and channels if available
+     */
+    public val username: String? = null,
+    /**
+     * Optional. First name of the other party in a private chat
+     */
+    public val first_name: String? = null,
+    /**
+     * Optional. Last name of the other party in a private chat
+     */
+    public val last_name: String? = null,
+)
 
 /**
  * This object represents a Telegram user or bot.
