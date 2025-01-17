2a9e7647fc7686996c371573cd03650a5041af64
Ruslan Ibragimov <ruslan@heapy.io>
2025-01-17 12:19:01 +0300

    Make sure resource closing happens only once

--- tgkotbot/src/main/kotlin/io/heapy/kotbot/infra/lifecycle/AutoClosableModule.kt
+++ tgkotbot/src/main/kotlin/io/heapy/kotbot/infra/lifecycle/AutoClosableModule.kt
@@ -2,10 +2,12 @@ package io.heapy.kotbot.infra.lifecycle
 
 import io.heapy.komok.tech.di.lib.Module
 import io.heapy.komok.tech.logging.Logger
+import java.util.concurrent.atomic.AtomicBoolean
 
 @Module
 open class AutoClosableModule : AutoCloseable {
     private val closable = mutableListOf<() -> Unit>()
+    private val used = AtomicBoolean(false)
 
     fun <T> addClosable(
         t: T,
@@ -21,15 +23,20 @@ open class AutoClosableModule : AutoCloseable {
     }
 
     override fun close() {
-        closable
-            .reversed()
-            .forEach { closeFunction ->
-                try {
-                    closeFunction()
-                } catch (e: Exception) {
-                    log.error("Error while closing resource", e)
+        if (used.compareAndSet(false, true)) {
+            closable
+                .reversed()
+                .forEach { closeFunction ->
+                    try {
+                        closeFunction()
+                    } catch (e: Exception) {
+                        log.error("Error while closing resource", e)
+                    }
                 }
-            }
+            log.info("Closed all resources")
+        } else {
+            log.warn("Already closed")
+        }
     }
 
     private companion object : Logger()
