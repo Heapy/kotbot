08d549e43070bb9d8d4e97f84bd0b31b21335995
Ruslan Ibragimov <ruslan@heapy.io>
2025-01-17 11:53:31 +0300

    Do not store default http client requests/responses

--- tgkotbot/src/main/kotlin/io/heapy/kotbot/infra/HttpClientModule.kt
+++ tgkotbot/src/main/kotlin/io/heapy/kotbot/infra/HttpClientModule.kt
@@ -1,7 +1,6 @@
 package io.heapy.kotbot.infra
 
 import io.heapy.komok.tech.di.lib.Module
-import io.heapy.kotbot.infra.http_client.HttpRequestLogger
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
@@ -12,10 +11,6 @@ import kotlinx.serialization.json.Json
 open class HttpClientModule {
     open val httpClient: HttpClient by lazy {
         HttpClient(CIO) {
-            install(HttpRequestLogger) {
-                // TODO: Provide service to store in DB
-            }
-
             install(ContentNegotiation) {
                 json(json = Json {
                     ignoreUnknownKeys = true
