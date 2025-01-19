ca5974fb16601a1aec1452c92cfead55a140104d
Ruslan Ibragimov <ruslan@heapy.io>
2025-01-19 10:51:23 +0300

    Add ParseModes for sending messages

---
+++ core/src/main/kotlin/io/heapy/kotbot/bot/model/ParseModes.kt
@@ -0,0 +1,10 @@
+package io.heapy.kotbot.bot.model
+
+/**
+ * See [#formatting-options](https://core.telegram.org/bots/api/#formatting-options) for details.
+ */
+public enum class ParseMode {
+    HTML,
+    Markdown,
+    MarkdownV2,
+}
